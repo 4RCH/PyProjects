dfc4d52625ae347e0822a2bbebacd56f27d779ec
4RCH <gfdaza@hotmail.co.uk>
2024-08-22 20:04:25 +0100

    Added asteroids and shooting

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -10,7 +11,7 @@ class Player(CircleShape):
         self.rotation = 0
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "orange", self.triangle(), 2)
 
     # in the player class
     def triangle(self):
@@ -32,6 +33,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -39,3 +42,7 @@ class Player(CircleShape):
     def move(self,dt):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
